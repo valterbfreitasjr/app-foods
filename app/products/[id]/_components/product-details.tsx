8a1ca9ea9233e3e38334a7215147935fdc20cf5a
Valter B Freitas Jr <valter191@hotmail.com>
2024-05-14 17:43:58 -0300

    feat: details component

--- app/products/[id]/_components/product-details.tsx
+++ app/products/[id]/_components/product-details.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import DiscountBadge from "@/app/_components/discount-badge";
+import { Button } from "@/app/_components/ui/button";
 import {
   calculateProductTotalPrice,
   formatCurrency,
 } from "@/app/_helpers/price";
 import { Prisma } from "@prisma/client";
+import { ChevronLeftIcon, ChevronRight, ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
+import { useState } from "react";
 
 interface ProductDetailsProps {
   product: Prisma.ProductGetPayload<{
@@ -15,6 +20,18 @@ interface ProductDetailsProps {
 }
 
 const ProductDetails = ({ product }: ProductDetailsProps) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleClickIncreaseQuantity = () =>
+    setQuantity((quantity) => quantity + 1);
+
+  const handleClickDecreaseQuantity = () =>
+    setQuantity((quantity) => {
+      if (quantity === 1) return 1;
+
+      return quantity - 1;
+    });
+
   return (
     <div className="p-5">
       {/* Restaurant */}
@@ -37,8 +54,8 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
         {product.restaurant.name}
       </h1>
 
+      {/* Price and Quantity */}
       <div className="flex justify-between">
-        {/* Price */}
         <div>
           <div className=" flex items-center gap-2">
             <h2 className="text-xl font-semibold">
@@ -56,6 +73,21 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
               De: {formatCurrency(Number(product.price))}
             </p>
           )}
+        </div>
+
+        <div className="flex items-center gap-3">
+          <Button
+            size="icon"
+            variant="ghost"
+            className="border border-solid border-muted-foreground"
+            onClick={handleClickDecreaseQuantity}
+          >
+            <ChevronLeftIcon />
+          </Button>
+          <span className="w-4">{quantity}</span>
+          <Button size="icon" onClick={handleClickIncreaseQuantity}>
+            <ChevronRightIcon />
+          </Button>
         </div>
       </div>
     </div>
