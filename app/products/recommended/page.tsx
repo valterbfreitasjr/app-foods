1cce02b6f9522d651fc1683c49b5db4c3af4e39d
Valter B Freitas Jr <valter191@hotmail.com>
2024-05-21 01:24:06 -0300

    feat: add product recommended page

---
+++ app/products/recommended/page.tsx
@@ -0,0 +1,35 @@
+import Header from "@/app/_components/header";
+import ProductItem from "@/app/_components/product-item";
+import { db } from "@/app/_lib/prisma";
+
+const RecommendedProducts = async () => {
+  const products = await db.product.findMany({
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
+  return (
+    <>
+      <Header />
+      <div className="px-5 py-6">
+        <h2 className="mb-6 text-lg font-semibold">Produtos recomendados</h2>
+        <div className="grid grid-cols-2 gap-6">
+          {products.map((product) => (
+            <ProductItem
+              product={product}
+              key={product.id}
+              className="min-w-full max-w-full"
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RecommendedProducts;
