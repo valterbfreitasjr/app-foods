d97ccfc28d056b4dd635bc1809b0f719a8f3c25b
Valter B Freitas Jr <valter191@hotmail.com>
2024-05-21 01:22:19 -0300

    feat: add products categories page

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="flex justify-between gap-3 px-5 pt-5">
-      <Image src="/logo.png" alt="eFood" height={30} width={100} />
+      <div className="relative h-[30px] w-[100px]">
+        <Link href="/">
+          <Image src="/logo.png" alt="eFood" fill />
+        </Link>
+      </div>
       <Button
         size="icon"
         variant="outline"
