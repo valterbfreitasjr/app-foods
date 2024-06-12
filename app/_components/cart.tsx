0134f34d3fe7c1d8886c95d6acc188d495ac762c
Valter B Freitas Jr <valter191@hotmail.com>
2024-06-12 00:59:41 -0300

    feat: ensure order can be created

--- app/_components/cart.tsx
+++ app/_components/cart.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { use, useContext, useState } from "react";
 import { CartContext } from "../_context/cart";
 import CartItem from "./cart-item";
 import { Card, CardContent } from "./ui/card";
@@ -9,12 +9,27 @@ import { Loader2 } from "lucide-react";
 import { createOrder } from "../_actions/order";
 import { OrderStatus } from "@prisma/client";
 import { useSession } from "next-auth/react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "./ui/alert-dialog";
 
 const Cart = () => {
   const { products, subtotalPrice, totalDiscounts, totalPrice, clearCart } =
     useContext(CartContext);
   const { data } = useSession();
   const [submitLoading, setSubmitLoading] = useState(false);
+  const [isConfirmDialogOpen, setIsConfirmDialogOpen] = useState(false);
+
+  const handleClickConfirmOrder = () => {
+    setIsConfirmDialogOpen(true);
+  };
 
   const handleFinishOrderClick = async () => {
     try {
@@ -38,7 +53,6 @@ const Cart = () => {
         },
       });
       clearCart();
-      // 1:25
     } catch (error) {
       console.log(error);
     } finally {
@@ -47,70 +61,99 @@ const Cart = () => {
   };
 
   return (
-    <div className="flex h-full flex-col py-5">
-      {products.length > 0 ? (
-        <>
-          <div className="flex-auto space-y-2">
-            {products.map((product) => (
-              <CartItem cartProducts={product} key={product.id} />
-            ))}
-          </div>
+    <>
+      <div className="flex h-full flex-col py-5">
+        {products.length > 0 ? (
+          <>
+            <div className="flex-auto space-y-2">
+              {products.map((product) => (
+                <CartItem cartProducts={product} key={product.id} />
+              ))}
+            </div>
 
-          {/* Totais */}
-          <div className="mt-6">
-            <Card>
-              <CardContent className="space-y-2 p-5">
-                <div className="mt-2 flex items-center justify-between text-xs">
-                  <span className="text-muted-foreground ">Subtotal</span>
-                  <span>{formatCurrency(subtotalPrice)}</span>
-                </div>
+            {/* Totais */}
+            <div className="mt-6">
+              <Card>
+                <CardContent className="space-y-2 p-5">
+                  <div className="mt-2 flex items-center justify-between text-xs">
+                    <span className="text-muted-foreground ">Subtotal</span>
+                    <span>{formatCurrency(subtotalPrice)}</span>
+                  </div>
 
-                <Separator />
+                  <Separator />
 
-                <div className="flex items-center justify-between text-xs">
-                  <span className="text-muted-foreground ">Descontos</span>
-                  <span>- {formatCurrency(totalDiscounts)}</span>
-                </div>
+                  <div className="flex items-center justify-between text-xs">
+                    <span className="text-muted-foreground ">Descontos</span>
+                    <span>- {formatCurrency(totalDiscounts)}</span>
+                  </div>
 
-                <Separator />
+                  <Separator />
 
-                <div className="mt-2 flex items-center justify-between text-xs">
-                  <span className="text-muted-foreground ">Entrega</span>
-                  {Number(products[0]?.restaurant.deliveryFee) === 0 ? (
-                    <span className="uppercase text-primary">grátis</span>
-                  ) : (
-                    <p>
-                      {formatCurrency(
-                        Number(products[0]?.restaurant.deliveryFee),
-                      )}
-                    </p>
-                  )}
-                </div>
+                  <div className="mt-2 flex items-center justify-between text-xs">
+                    <span className="text-muted-foreground ">Entrega</span>
+                    {Number(products[0]?.restaurant.deliveryFee) === 0 ? (
+                      <span className="uppercase text-primary">grátis</span>
+                    ) : (
+                      <p>
+                        {formatCurrency(
+                          Number(products[0]?.restaurant.deliveryFee),
+                        )}
+                      </p>
+                    )}
+                  </div>
 
-                <Separator />
+                  <Separator />
 
-                <div className="flex items-center justify-between text-xs font-semibold">
-                  <span>Total</span>
-                  <span>{formatCurrency(totalPrice)}</span>
-                </div>
-              </CardContent>
-            </Card>
-          </div>
+                  <div className="flex items-center justify-between text-xs font-semibold">
+                    <span>Total</span>
+                    <span>{formatCurrency(totalPrice)}</span>
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
 
-          {/* Finalizar Pedido */}
-          <Button
-            className="mt-6 w-full"
-            onClick={handleFinishOrderClick}
-            disabled={submitLoading}
-          >
-            {submitLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-            Finalizar pedido
-          </Button>
-        </>
-      ) : (
-        <h2 className="text-center font-medium">Não há produtos na sacola.</h2>
-      )}
-    </div>
+            {/* Finalizar Pedido */}
+            <Button
+              className="mt-6 w-full"
+              onClick={handleClickConfirmOrder}
+              disabled={submitLoading}
+            >
+              {submitLoading && (
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              )}
+              Finalizar pedido
+            </Button>
+          </>
+        ) : (
+          <h2 className="text-center font-medium">
+            Não há produtos na sacola.
+          </h2>
+        )}
+      </div>
+
+      <AlertDialog
+        open={isConfirmDialogOpen}
+        onOpenChange={setIsConfirmDialogOpen}
+      >
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Deseja finalizar seu pedido?</AlertDialogTitle>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel
+              onClick={() => {
+                setIsConfirmDialogOpen(false);
+              }}
+            >
+              Não
+            </AlertDialogCancel>
+            <AlertDialogAction onClick={handleFinishOrderClick}>
+              Sim, finalizar pedido.
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 };
 
